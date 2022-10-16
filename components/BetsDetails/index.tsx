734ddc183fe6f38c8066676ca90b47a8da00a8a9
JaumDarkz <viniciuspkfr@gmail.com>
2022-10-16 05:44:18 -0300

    feat: finish static page

--- components/BetsDetails/index.tsx
+++ components/BetsDetails/index.tsx
@@ -5,6 +5,8 @@ import styles from './styles.module.scss'
 import back from './assets/back.svg'
 import refresh from './assets/refresh.svg'
 import inspect from './assets/inspect.svg'
+import copy from './assets/copy.svg'
+import arrow from './assets/arrow.svg'
 
 type dataProps = {
   betHashData: string,
@@ -71,44 +73,96 @@ const BetsDetails = ({...props}: dataProps) => {
           </div>
         </div>
 
+        <div className={styles.overviewLine} />
+
         <div className={styles.copyContainer}>
           <div className={styles.title}>
             Bet Hash
           </div>
 
-          <div className={styles.data}>
-            {props.betHashData}
+          <div className={styles.centerData}>
+            <div className={styles.rowContainer}>
+              <div className={styles.imageContainer}>
+                <Image src={copy} alt='Copy' onClick={() => navigator.clipboard.writeText(props.betHashData)}/>
+              </div>
+
+              <div className={styles.centerData}>
+                <div className={styles.data}>
+                  {props.betHashData}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+
+        <div className={styles.overviewLine} />
 
         <div className={styles.noCopyContainer}>
           <div className={styles.title}>
             Timestamp
           </div>
 
-          <div className={styles.data}>
-            {props.timestampData}
+          <div className={styles.centerData}>
+            <div className={styles.data}>
+              {props.timestampData}
+            </div>
           </div>
         </div>
+
+        <div className={styles.overviewLine} />
 
         <div className={styles.copyContainer}>
           <div className={styles.title}>
             Bet Number
           </div>
 
-          <div className={styles.data}>
-            {props.betNumberData}
+          <div className={styles.centerData}>
+            <div className={styles.rowContainer}>
+              <div className={styles.imageContainer}>
+                <Image src={copy} alt='Copy' onClick={() => navigator.clipboard.writeText(props.betNumberData)}/>
+              </div>
+
+              <div className={styles.centerData}>
+                <div className={styles.data}>
+                  {props.betNumberData}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+
+        <div className={styles.overviewLine} />
 
         <div className={styles.noCopyContainer}>
           <div className={styles.title}>
             Associated Value
           </div>
 
-          <div className={styles.data}>
-            {props.associatedValueData}
+          <div className={styles.centerData}>
+            <div className={styles.data}>
+              {props.associatedValueData}
+            </div>
           </div>
+        </div>
+      </div>
+
+      <div className={styles.resultsDetailsContainer}>
+        <div className={styles.title}>
+          Results
+        </div>
+
+        <div className={styles.arrow}>
+          <Image src={arrow} alt='Expand' />
+        </div>
+      </div>
+
+      <div className={styles.resultsDetailsContainer}>
+        <div className={styles.title}>
+          Details
+        </div>
+
+        <div className={styles.arrow}>
+          <Image src={arrow} alt='Expand' />
         </div>
       </div>
     </div>
