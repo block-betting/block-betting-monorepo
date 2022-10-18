2591c951d0c3ca61de396cd3cd0db5d605ebd9a7
JaumDarkz <viniciuspkfr@gmail.com>
2022-10-18 00:42:26 -0300

    fix: data positioning

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,14 +3,15 @@ import Image from 'next/image'
 import styles from './styles.module.scss'
 
 import logo from 'public/assets/brand/logo.svg'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
-        <div className={styles.logoContainer}>
+        <Link href={'/'}><div className={styles.logoContainer}>
           <Image src={logo} alt='Logo' />
-        </div>
+        </div></Link>
       </div>
     </div>
   )
