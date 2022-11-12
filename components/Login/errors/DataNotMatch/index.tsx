06533af4342fceaa03159c40ca4527231e0121af
JaumDarkz <viniciuspkfr@gmail.com>
2022-11-11 23:59:17 -0300

    feat: add login page

---
+++ components/Login/errors/DataNotMatch/index.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+
+import styles from './styles.module.scss'
+
+import signal from './assets/signal.svg'
+import arrow from './assets/arrow.svg'
+
+const DataNotMatch = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.arrow}>
+        <Image src={arrow} alt='Arrow' />
+      </div>
+      <div className={styles.row}>
+        <div className={styles.signal}>
+          <Image src={signal} alt='Error' />
+        </div>
+
+        <div className={styles.error}>
+          The email or password you entered does not match our records. Check and try again
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DataNotMatch
