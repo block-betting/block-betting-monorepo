11a8be4e62872e92339af4304e3467418820a32e
JaumDarkz <viniciuspkfr@gmail.com>
2022-11-16 18:32:58 -0300

    feat: add recovery page

---
+++ pages/changepassword.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const ChangePassword: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>BlockBetting</title>
+        <meta name="Block Betting" content="Descentralized bets and more." />
+        <link rel="icon" href="../public/brand/logo" />
+      </Head>
+    </>
+  )
+}
+
+export default ChangePassword
