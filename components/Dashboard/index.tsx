e5739276898dfc59e4e30a6efba0783a80aa1b24
JaumDarkz <viniciuspkfr@gmail.com>
2022-11-18 01:54:47 -0300

    feat: add bets to dashboard

--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -1,10 +1,13 @@
-import Sidebar from '../Sidebar'
 import styles from './styles.module.scss'
+
+import CardGrid from './components/CardGrid'
+import Sidebar from '../Sidebar'
 
 const DashboardComponent = () => {
   return (
     <div className={styles.container}>
       <Sidebar />
+      <CardGrid />
     </div>
   )
 }
